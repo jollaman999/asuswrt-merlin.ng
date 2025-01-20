590e56437a6a60de0d5465381232722595d7f70f
jollaman999 <admin@jollaman999.com>
2025-01-21 02:13:45 +0900

    kernel41-6750: tcp: tcp_tso_autosize() minimum is one packet
    By making sure sk->sk_gso_max_segs minimal value is one,
    and sysctl_tcp_min_tso_segs minimal value is one as well,
    tcp_tso_autosize() will return a non zero value.
    
    We can then revert 843925f
    ("tcp: Do not apply TSO segment limit to non-TSO packets")
    and save few cpu cycles in fast path.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Neal Cardwell <ncardwell@google.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Acked-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- release/src-rt-5.02axhnd.675x/kernel/linux-4.1/net/core/sock.c
+++ release/src-rt-5.02axhnd.675x/kernel/linux-4.1/net/core/sock.c
@@ -1639,6 +1639,8 @@ EXPORT_SYMBOL_GPL(sk_clone_lock);
 
 void sk_setup_caps(struct sock *sk, struct dst_entry *dst)
 {
+	u32 max_segs = 1;
+
 	__sk_dst_set(sk, dst);
 	sk->sk_route_caps = dst->dev->features;
 	if (sk->sk_route_caps & NETIF_F_GSO)
@@ -1655,6 +1657,7 @@ void sk_setup_caps(struct sock *sk, struct dst_entry *dst)
 			max_segs = max_t(u32, READ_ONCE(dst->dev->gso_max_segs), 1);
 		}
 	}
+	sk->sk_gso_max_segs = max_segs;
 }
 EXPORT_SYMBOL_GPL(sk_setup_caps);
 
